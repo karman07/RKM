922930ce32c9ab9d811e3a6947106e9b035a9196
karman07 <karmansingharora01@gmail.com>
2026-07-18 10:36:31 +0530

    done

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,8 +19,8 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
-      <body className="min-h-full flex flex-col">
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`} suppressHydrationWarning>
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
         <NotificationsProvider />
         {children}
         <Toaster position="top-center" richColors />
